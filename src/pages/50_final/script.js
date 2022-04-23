3e67e326844b76d5a3d31fb00000e6d504d199ac
Nilo César <e_maildonilo@yahoo.com.br>
2022-04-22 21:49:50 -0300

    complete mercadobitcoin

--- src/pages/50_final/script.js
+++ src/pages/50_final/script.js
@@ -1,11 +1,7 @@
 events.on('ready', function () {
   
-  // scorm.saveObject("question", 0);
-  // scorm.saveObject("quizStatus", {"itens":[]});
-  // scorm.saveObject("quizes", '');
-  // scorm.saveObject("avatar_selecionado", '');
-
   scorm.setCompleted();
+  
   $('.triangulo').on('click', function() {
     scorm.set("cmi.suspend_data", "{}");
     navigate.goto('02_city');
